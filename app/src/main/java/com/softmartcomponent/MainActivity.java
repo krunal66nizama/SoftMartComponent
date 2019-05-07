66c79b45cef970a7d304bb38e70d38b8b9425c30
Krunal Nizama <krunal66nizama@gmail.com>
2019-05-07 23:42:26 +0530

    price set and get done

--- app/src/main/java/com/softmartcomponent/MainActivity.java
+++ app/src/main/java/com/softmartcomponent/MainActivity.java
@@ -1,24 +1,50 @@
 package com.softmartcomponent;
 
+import android.content.Context;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    com.component.TextComponent textComponent;
-    com.component.EdittextComponent edtComponent , edtComponent1;
+    Context context;
+    TextView txt;
+    Button btn;
+    com.component.EdittextComponent edtComponent, edtComponent1;
+
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
 
-        textComponent = findViewById(R.id.textComponent);
+        txt = findViewById(R.id.txt);
+        btn = findViewById(R.id.btn);
         edtComponent = findViewById(R.id.edtComponent);
         edtComponent1 = findViewById(R.id.edtComponent1);
-        //textComponent.setBackground(getResources().getDrawable(R.drawable.black));
-        textComponent.setBackgroundColor(getResources().getColor(R.color.black));
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                txt.setText(edtComponent.getValue());
+                /*if (edtComponent.getText().length() > 0) {
+                    String cleanString = edtComponent.getText().toString().replaceAll("[₹$,.]", "");
+                    cleanString = cleanString.replaceAll("\\s", "");
+
+                    double parsed = Double.parseDouble(cleanString);
+                    txt.setText(String.valueOf(parsed / 100));
+                    txt.setText(edtComponent.getValue());
+                }*/
+            }
+        });
     }
 }
--- component/src/main/java/com/component/EdittextComponent.java
+++ component/src/main/java/com/component/EdittextComponent.java
@@ -3,45 +3,98 @@ package com.component;
 import android.content.Context;
 import android.graphics.Rect;
 import android.support.v7.widget.AppCompatEditText;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
+
+import java.text.NumberFormat;
 
 public class EdittextComponent extends AppCompatEditText {
     Context context;
+
     public EdittextComponent(Context context) {
         super(context);
         this.context = context;
-        super.setBackground(context.getResources().getDrawable(R.drawable.focused_removed));
-        super.setPadding(10 , 10 , 10 , 10);
-        super.setTextSize(15);
+        setDefault();
+
     }
 
     public EdittextComponent(Context context, AttributeSet attrs) {
         super(context, attrs);
         this.context = context;
-        super.setBackground(context.getResources().getDrawable(R.drawable.focused_removed));
-        super.setPadding(10 , 10 , 10 , 10);
-        super.setTextSize(15);
+        setDefault();
     }
 
     public EdittextComponent(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         this.context = context;
-        super.setBackground(context.getResources().getDrawable(R.drawable.focused_removed));
-        super.setPadding(10 , 10 , 10 , 10);
-        super.setTextSize(15);
+        setDefault();
+    }
+
+    String current = "";
+
+    public void setDefault() {
+        super.setPadding(20, 20, 20, 20);
+        super.setTextSize(20);
+        super.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+
+        super.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().equals(current)) {
+                    removeTextChangedListener(this);
+
+                    String cleanString = s.toString().replaceAll("[₹$,.]", "");
+                    cleanString = cleanString.replaceAll("\\s", "");
+
+                    double parsed = Double.parseDouble(cleanString);
+                    String formatted = NumberFormat.getCurrencyInstance().format((parsed / 100));
+
+                    current = formatted;
+                    setText(formatted);
+                    setSelection(formatted.length());
+
+                    addTextChangedListener(this);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    public String getValue() {
+
+        String value = "";
+        if (getText().length() > 0) {
+            String cleanString = getText().toString().replaceAll("[₹$,.]", "");
+            cleanString = cleanString.replaceAll("\\s", "");
+
+            double parsed = Double.parseDouble(cleanString);
+            value = String.valueOf(parsed / 100);
+        }
+
+        return value;
     }
 
     @Override
     protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
         super.onFocusChanged(focused, direction, previouslyFocusedRect);
-        if (focused){
+        if (focused) {
             super.setHint("Focused");
             super.setBackground(context.getResources().getDrawable(R.drawable.focused));
         } else {
-            super.setHint("focus removed");
+            super.setHint("Focus Removed");
             super.setBackground(context.getResources().getDrawable(R.drawable.focused_removed));
         }
-        super.setPadding(10 , 10 , 10 , 10);
-        super.setTextSize(15);
     }
 }
